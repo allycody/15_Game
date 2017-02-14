995b6d3a8e6c2823c02a282c190acb93d9fe9a58
Ally Cody <allysoncody2015@u.northwestern.edu>
2017-02-14 15:12:22 -0500

    can move tile or group of tiles in any direction, needs comments and refactoring

--- functionality.js
+++ functionality.js
@@ -24,12 +24,12 @@ function shuffle(array) {
 
 function setTiles(){
   var tiles = document.getElementsByClassName("square")
-  console.log(tiles)
+  // console.log(tiles)
   var myTiles = [...tiles]
-  console.log("my: ", myTiles)
+  // console.log("my: ", myTiles)
   var tilesArr = Array.from(tiles)
-  console.log(Array.isArray(tiles))
-  console.log(tilesArr)
+  // console.log(Array.isArray(tiles))
+  // console.log(tilesArr)
   var shuffledNums = shuffle([1, 2, 3, 4, 5, 6, 7, 8])
   for(let i = 0; i<8; i++){
 
@@ -41,39 +41,44 @@ function setTiles(){
 
 }
 
-// function move(event){
-//   console.log(event.target.id)
-//   var box = event.target
-//   var strCoords = event.target.id
-//   var coords = [...event.target.id.split('-')].map(coord => parseInt(coord))
-//   console.log(coords)
-//   var slidable = canMove(coords)
-//   console.log("slidable", slidable)
-//   if(slidable){
-//     //moveRight(box, strCoords)
-//   }
-// }
-
 
 function move(event){
-  console.log(event.target.id)
+  // console.log(event.target.id)
   let tile = event.target
   let strCoords = event.target.id
   let coords = [...event.target.id.split('-')].map(coord => parseInt(coord))
-  console.log(coords)
+  // console.log(coords)
 
   let empty = document.getElementById("empty")
   let emptyCoords = empty.className.split(" ")[1].split("-").map(coord => parseInt(coord))
   let slidable = canMove(coords, emptyCoords)
-  console.log("slidable", slidable)
+  console.log("num to slide", slidable[2])
   if(slidable){
-    moveRight(...slidable, tile, strCoords)
-    resetClass(...slidable, tile, coords)
+    let tilesToSlide = [] 
+    if(slidable[2] > 1){
+      tilesToSlide.push(tile)
+      for(let i = 1; i < slidable[2]; i++){
+        let siblingCoords = (slidable[0] === "left" || slidable[0] === "right") ? [coords[0], (coords[1] + (slidable[1]) * i)] : [(coords[0] + (slidable[1]) * i), coords[1]]
+        console.log("siblingCoords: ", siblingCoords)
+        siblingCoords = siblingCoords.join("-").toString()
+        tilesToSlide.push(document.getElementById(siblingCoords))
+      }
+      console.log("TILES TO SLIDE: ", tilesToSlide)
+      for(let i = tilesToSlide.length - 1; i >= 0; i--){
+        move({target: tilesToSlide[i]})
+      }
+    }
+    else updateTiles(slidable.slice(0, 2), tile, strCoords, coords, empty, emptyCoords)
   }
 }
 
+function updateTiles(slidable, tile, strCoords, coords, empty, emptyCoords){
+    moveDir(...slidable, tile, strCoords)
+    resetClass(...slidable, tile, coords, empty, emptyCoords)
+}
+
 function canMove(coords, emptyCoords){
-  console.log(emptyCoords)
+  // console.log(emptyCoords)
   let y_dif = emptyCoords[0] - coords[0]
   let x_dif = emptyCoords[1] - coords[1]
   if(y_dif && x_dif){
@@ -82,24 +87,56 @@ function canMove(coords, emptyCoords){
   }
   // can move along y-axis
   else if(y_dif){
-    return (y_dif < 0 ? ["up", y_dif] : ["down", y_dif])
+    return (y_dif < 0 ? ["up", -1, Math.abs(y_dif)] : ["down", 1, Math.abs(y_dif)])
   }
   // can move along x-axis
-  else return (x_dif < 0 ? ["left", x_dif] : ["right", x_dif])
+  else return (x_dif < 0 ? ["left", -1, Math.abs(x_dif)] : ["right", 1, Math.abs(x_dif)])
 }
 
-function moveRight(dir, inc, b, strCoords){
+function moveDir(dir, inc, b, strCoords){
+  console.log("MOVE ", dir)
   console.log("b", b)
   console.log("strCoords: ", strCoords)
-  console.log(b.style)
-  console.log("width: ", b.offsetWidth)
-  let box = $("#" + strCoords);
-  console.log("box", box)
-  let width = box.outerWidth(true)
+  console.log("currTrans: ", b.style.transform)
+  let currTrans = b.style.transform.split(" ")
+  console.log("currTrans: ", currTrans)
+  let transCoords = currTrans.map(element => parseFloat(element.match(/\-?\d+\.+\d+/g)))
 
-  b.style.transform = 'translateX(' + width + 'px)'
-  document.getElementById("empty").className = "empty 3-2"
-  console.log("class: ", document.getElementById("empty").className)
+  //parseFloat(b.style.transform.match(/\-?\d+\.+\d+/g)) || 0
+  console.log("regex return: ", transCoords)
+  let box = $("#" + strCoords);
+  // console.log("box", box)
+  let width = box.outerWidth(true) 
+  let moveX = transCoords[0] || 0;
+  let moveY = transCoords[1] || 0;
+
+
+ // could set var translate to translateX or translateY 
+ // and var move to distance based on dir
+ // then call b.style.transform once ?
+
+  // if(dir === "right"){
+  //   move = currTrans + width
+  //   b.style.transform = 'translateX(' + move + 'px)'
+  // }
+  // else if(dir === "left") {
+  //   console.log("in left")
+  //   move = currTrans - width
+  //   console.log("move distance: ", move)
+  //   b.style.transform = 'translateX(' + move + 'px)'
+  // }
+
+
+  if(dir === "right" || dir === "left"){
+    moveX = dir === "right" ? (moveX + width) : (moveX - width) 
+  }
+
+  if(dir === "down" || dir === "up"){
+    moveY = dir === "down" ? (moveY + width) : (moveY - width) 
+  }
+  console.log("TOTAL TRANS: ", 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)')
+
+  b.style.transform = 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)'
 
 //   $(function() {
 // let box = $("#" + coords);
@@ -110,18 +147,26 @@ function moveRight(dir, inc, b, strCoords){
 // });
 }
 
-function resetClass(dir, inc, tile, coords){
+function resetClass(dir, inc, tile, coords, empty, emptyCoords){
   if(dir === "up" || dir === "down"){
     coords[0] += inc
+    emptyCoords[0] -= inc
   }
 
   else if(dir === "left" || dir === "right"){
     coords[1] += inc
+    emptyCoords[1] -= inc
   }
 
   coords = coords.join("-").toString()
+  emptyCoords = emptyCoords.join("-").toString()
   tile.id = coords
+
+  empty.className = "empty " + emptyCoords
 }
+
+// might make more sense instead of converting to nums and incrementing back to string
+// we could pull the class/id off the element in string format and just swap them
 
 setTiles()
 
