25a21d29bdd981ba44f37fab270f6729d9937c0c
Ally Cody <allysoncody2015@u.northwestern.edu>
2017-02-17 14:18:51 -0500

    now actually creates solvable puzzles

--- functionality.js
+++ functionality.js
@@ -1,11 +1,10 @@
-// find target
-// find empty
-// compare target to empty
-//
-
 let rows = 3
 let boardSize = 9
 let sizeClass = 3
+let nums = [...Array(boardSize).keys()].slice(1)
+nums.push(0)
+let shuffledNums = nums.slice(0)
+let emptyI = nums.indexOf(0)
 
 function setBoardSize(size){
 
@@ -13,6 +12,10 @@ function setBoardSize(size){
   if(rows !== size){
     rows = size;
     boardSize = size * size;
+    nums = [...Array(boardSize).keys()].slice(1)
+    nums.push(0)
+    shuffledNums = nums.slice(0)
+    emptyI = nums.indexOf(0)
 
     document.getElementById("board").innerHTML = null
 
@@ -20,62 +23,146 @@ function setBoardSize(size){
   }
 }
 
-function shuffle(array) {
+function makeMove(neighbors){
+  let legitimateNeighbors = neighbors.filter(element => (element >= 0 && element < boardSize))
+  let dir = Math.floor(Math.random() * 4)
+  let tileInd = legitimateNeighbors[Math.floor(Math.random() * legitimateNeighbors.length)]
+  let spacesMoved = tileInd - emptyI
 
-  let currentIndex = array.length, temporaryValue, randomIndex;
 
-  // If the number of rows is even and the empty square is in the last row
-  // the number of swaps we make in the array must be even
-  // but array.length is odd when rows is even
-  if(!rows%2) currentIndex ++
+  let valA = shuffledNums[tileInd]
+  shuffledNums[tileInd] = shuffledNums[emptyI]
+  shuffledNums[emptyI] = valA
+  console.log("unsaved shuffle: ", shuffledNums)
+  neighbors.forEach((element, i) => {neighbors[i] = neighbors[i] + spacesMoved})
+  emptyI += spacesMoved
+  //console.log("updated emptyI: ", emptyI)
+  //console.log("NEIGHBORS UPDATED: ", neighbors)
+
+
+  // switch(dir){
+  //   case 0:
+  //     // move up
+  //     let valA = array[tileInd]
+  //     array[tileInd] = array[tileInd - rows]
+  //     array[tileInd - rows] = valA
+  //     break;
+
+  //   case 1:
+  //     // move down
+  //     let valA = array[tileInd]
+  //     array[tileInd] = array[tileInd + rows]
+  //     array[tileInd + rows] = valA
+
+  //   case 2:
+  //     // move left
+  //     let valA = array[tileInd]
+  //     array[tileInd] = array[tileInd - 1]
+  //     array[tileInd - 1] = valA
+
+  //   case 3:
+  //     // move right
+  //     let valA = array[tileInd]
+  //     array[tileInd] = array[tileInd + 1]
+  //     array[tileInd + 1] = valA
+  // }
+
+}
+
+function neighborsOfEmpty(){
+  let emptyI = nums.indexOf(0)
+  console.log("empty index", emptyI)
+  let neighbors = [emptyI + 1, emptyI - 1, emptyI + rows, emptyI - rows]
+  return neighbors
+}
+
+function shuffle(numTimes = 30){
+  //let neighbors = neighborsOfEmpty()
   
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+  console.log("empty index", emptyI)
+  let neighbors = [emptyI + 1, emptyI - 1, emptyI + rows, emptyI - rows]
+  console.log("NEIGHBS: ", neighbors)
+  //console.log("LEGIT: ", legitimateNeighbors)
+  while(numTimes > 0){
+    console.log("updated emptyI: ", emptyI)
+    console.log("NEIGHBORS UPDATED: ", neighbors)
+    makeMove(neighbors)
+    numTimes -- 
   }
 
-  return array;
+  console.log("SHUFFLED?", shuffledNums)
+
+  return shuffledNums
 }
+
+// function shuffle(array) {
+
+//   let currentIndex = array.length, temporaryValue, randomIndex;
+
+//   // If the number of rows is even and the empty square is in the last row
+//   // the number of swaps we make in the array must be even
+//   // but array.length is odd when rows is even
+//   if(!rows%2) {
+//     console.log("ROWS: ", rows)
+//     currentIndex ++
+//   }
+  
+//   let inversions = 0;
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     inversions++
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+
+//     // And swap it with the current element.
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+//   console.log("INV: ", inversions)
+
+//   return array;
+// }
 
 
 // Modify this later to allow for different board sizes
 // Initialize board
 function setTiles(){
-  let nums = [...Array(boardSize).keys()].slice(1)
+
+  console.log("NUMS ARR: ", nums, Array.isArray(nums))
+  // Randomize the numbers to assign to tiles
+  let shuffledNums = shuffle()
+  console.log("SUFFLED: ", shuffledNums)
 
   let board = document.getElementById("board")
   
   // Get all tiles
-  for(let i = 1; i <= rows; i++){
-    for(let j = 1; j <= rows; j++){
-      if(i === rows && j === rows) break;
+  for(let i = 0; i < rows; i++){
+    for(let j = 0; j < rows; j++){
+
       let square = document.createElement("DIV")
-      square.setAttribute("id",  i + '-' + j)
-      square.setAttribute("class", `${rows} square`)
-      square.addEventListener("click", move)
+      let num = shuffledNums[(j%rows) + (i*rows)]
+      let board = document.getElementById("board")
+      let width = parseFloat(window.getComputedStyle(board).width)/rows
+
+      console.log("NUM: ", num)
+      if(num){
+        square.setAttribute("id",  `${i}-${j}`)
+        square.setAttribute("class", `${rows} square`)
+        square.setAttribute("style", `top: ${(i * width)}px; left: ${(j * width)}px;`)
+        square.addEventListener("click", move)
+        square.innerHTML = num;
+      }
+
+      else{
+        square.setAttribute("id", "empty")
+        square.setAttribute("class", `empty ${i}-${j} ${rows}`)
+      }
+
       board.appendChild(square)
     } 
   }
-  let empty = document.createElement("DIV")
-  empty.setAttribute("id", "empty")
-  empty.setAttribute("class", "empty " + rows + "-" + rows)
-  board.appendChild(empty)
-  let tiles = document.getElementsByClassName("square")
-  let myTiles = [...tiles]
-
-  // Randomize the numbers to assign to tiles
-  let shuffledNums = shuffle(nums)
-
-  // Assign numbers to tiles
-  myTiles = myTiles.forEach(tile => tile.innerHTML = shuffledNums.pop().toString())
 
 }
 
@@ -132,7 +219,8 @@ function updateTiles(slidable, tile, strCoords, coords, empty, emptyCoords){
 }
 
 function canMove(coords, emptyCoords){
-  // console.log(emptyCoords)
+  console.log("empty coords: ", emptyCoords)
+  console.log("coords: ", coords)
   let y_dif = emptyCoords[0] - coords[0]
   let x_dif = emptyCoords[1] - coords[1]
   if(y_dif && x_dif){
@@ -154,10 +242,9 @@ function moveDir(dir, inc, b, strCoords){
   // Find the tile using its id 
   let tile = document.getElementById(strCoords)
 
-  // Find the tiles total width including margin
-  let style = window.getComputedStyle(tile)
-  console.log("STYLE: ", parseFloat(style.width) + (2 * parseFloat(style.marginLeft)))
 
+  // Find the tiles total width and height including margin
+  let style = window.getComputedStyle(tile)
   let width = parseFloat(style.width) + (2 * parseFloat(style.marginRight))
   let height = parseFloat(style.height) + (2 * parseFloat(style.marginTop))
 
@@ -166,8 +253,10 @@ function moveDir(dir, inc, b, strCoords){
   // Get the x and y coordinates currently applied to the div
   let currTrans = b.style.transform.split(" ")
 
+
   // Convert the current coordinates to an array of floats
   let transCoords = currTrans.map(element => parseFloat(element.match(/\-?\d+\.?\d+/g)))
+
 
   // Initilialize translated coordinates to 0 if not currently set
   let moveX = transCoords[0] || 0;
@@ -182,6 +271,7 @@ function moveDir(dir, inc, b, strCoords){
     moveY = dir === "down" ? (moveY + height) : (moveY - height) 
   }
   console.log("TOTAL TRANS: ", 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)')
+
 
   b.style.transform = 'translateX(' + moveX + 'px) translateY(' + moveY + 'px)'
 
